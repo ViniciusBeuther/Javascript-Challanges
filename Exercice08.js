de8a5418a14cf5d500e6565d44b39d1eb9b24f82
ViniciusBeuther <vinicius.beuther15@gmail.com>
2023-10-20 16:23:46 -0300

    Initial commit

---
+++ Exercice08.js
@@ -0,0 +1,22 @@
+// Interplanetary Library
+
+// Write a function that receive a string that contain a fullname and return a string with all names, except for the last one, abbreviated and the last name in uppercase before the abbreviations separated by comma
+
+const name = 'Isaac Larrubia Ferreira Pontes'
+const nameArr = name.split(' ')
+const finalName = []
+
+nameArr.forEach(name => {
+    if (nameArr[nameArr.length - 1] === name){
+        finalName.splice(0, 0, name.toUpperCase())
+    } else {
+        finalName.push(convertName(name + ',').toUpperCase())
+    }
+})
+
+function convertName(nameToConvert) {
+    return nameToConvert.replace(/([a-zA-Z]).*/, '$1.')
+}
+
+finalNameStr = finalName.join(' ')
+console.log(finalNameStr)
